4eecdbf2bc6f1116cc14243a7384daa709f0a21c
Ramaaditya <wayanramaaditya@gmail.com>
2024-05-16 21:32:37 +0800

    commit pertama

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,46 @@
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Sewa Mobil</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <span>Daftar Mobil</span></a>
+            </li>
+
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <span>Daftar pemesanan</span></a>
+            </li>
+
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <span>Daftar Pengembalian</span></a>
+            </li>
+
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <span>Logout</span></a>
+            </li>
+
+        </ul>
